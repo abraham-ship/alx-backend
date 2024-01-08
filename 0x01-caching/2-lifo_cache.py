81ae5b820fa7077bbb1010c90475142575719c21
abraham-ship <abuzsein3@gmail.com>
2024-01-08 20:39:00 +0000

    LIFO Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''LIFO Caching'''
+
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key = list(self.cache_data.keys())[-1]
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
