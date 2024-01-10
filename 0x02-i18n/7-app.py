a974e15884368a79cd86dc45019105baa6aed457
abraham-ship <abuzsein3@gmail.com>
2024-01-10 20:25:40 +0000

    Infer appropriate time zone

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+'''Infer appropriate time zone'''
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    '''check if locale is present and supported'''
+    requested_locale = request.args.get('locale')
+    if requested_locale and requested_locale in app.config['LANGUAGES']:
+        return requested_locale
+
+    if hasattr(g, 'user') and g.user and 'locale' in g.user and
+    g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+
+    header_locale = request.accept_languages.best_match
+    (app.config['LANGUAGES'])
+    if header_locale:
+        return header_locale
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+
+@babel.timezoneselector
+def get_timezone():
+    '''Find timezone parameter in URL parameters'''
+    requested_timezone = request.args.get('timezone')
+    if requested_timezone:
+        try:
+            get_timezone(requested_timezone)
+            return requested_timezone
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    if hasattr(g, 'user') and g.user and 'timezone' in g.user:
+        try:
+            get_timezone(g.user['timezone'])
+            return g.user['timezone']
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+    return 'UTC'
+
+
+def get_user(user_id):
+    '''returns a user dictionary or None if the ID cannot be found
+    or if login_as was not passed'''
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    '''use get_user to find a user if any,
+    and set it as a global on flask.g.user'''
+    user_id = request.args.get('login_as')
+    g.user = get_user(int(user_id)) if user_id else None
+
+
+@app.route('/')
+def index():
+    '''route'''
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
